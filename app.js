88eb431afa50eceba09d7a02dff6b14627e55804
WitherMite <43625516+WitherMite@users.noreply.github.com>
2025-08-04 11:35:01 -0500

    Serve css with ejs files

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes/router");
+const path = require("node:path");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", router);
 
 app.use((err, req, res, next) => {
